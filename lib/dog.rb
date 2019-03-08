0a926dd5a22ae16aa6dcbb5bf868266cd871a22c
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-08 02:40:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,14 @@
 class Dog 
   
-  def initialize(breed="Mutt")
+  def initialize(name, breed="Mutt")
     @breed = breed
+    @name = name 
+  end 
+  def name=(name)
+    @name = name 
+  end 
+  def name
+    @name 
   end 
   def breed=(breed)
     @breed = breed 
